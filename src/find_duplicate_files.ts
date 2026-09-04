7a1e82ea52770bd8c6f421d08d9f76a4743722cf
Aayush Singh <aayushs311@gmail.com>
2026-09-05 00:12:20 +0530

    Revert streaming SHA-256 file hashing

--- src/find_duplicate_files.ts
+++ src/find_duplicate_files.ts
@@ -3,16 +3,13 @@ import type { FilePath } from './types.ts';
 import { Buckets } from './buckets.ts';
 import { walk } from './walk.ts';
 
-export async function findDuplicateFiles(
-  directory: FilePath,
-  categorizers: Categorizer[],
-) {
+export function findDuplicateFiles(directory: FilePath, categorizers: Categorizer[]) {
   let buckets = new Buckets();
 
   buckets.add('initial', walk(directory));
 
   for (const categorizer of categorizers) {
-    buckets = (await categorizer.rebucket(buckets)).removeNonDuplicates();
+    buckets = categorizer.rebucket(buckets).removeNonDuplicates();
   }
 
   return buckets;

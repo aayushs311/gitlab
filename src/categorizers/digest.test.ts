cae9c01f12e06b3d010c850ecf68dcaa5b3d9e9a
Aayush Singh <aayushs311@gmail.com>
2026-09-05 00:12:51 +0530

    Revert streaming SHA-256 file hashing

--- src/categorizers/digest.test.ts
+++ src/categorizers/digest.test.ts
@@ -26,11 +26,11 @@ function makeFiles(
 }
 
 describe('DigestCategorizer', () => {
-  test('hashes files correctly', async () => {
+  test('hashes files correctly', () => {
     const directory = mkdtempSync(`${tmpdir()}${sep}`);
     const categorizer = new DigestCategorizer();
 
-    assert.deepStrictEqual(Array.from(await categorizer.rebucket(makeFiles(directory, {
+    assert.deepStrictEqual(Array.from(categorizer.rebucket(makeFiles(directory, {
       empty: '',
       another_empty: '',
       a: 'a',

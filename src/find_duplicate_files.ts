e23b51e4fc80cd883c534a10b2d3718fd03eb2bf
Aayush Singh <aayushs311@gmail.com>
2026-09-05 00:10:13 +0530

    Use streaming SHA-256 file hashing

--- src/find_duplicate_files.ts
+++ src/find_duplicate_files.ts
@@ -3,13 +3,16 @@ import type { FilePath } from './types.ts';
 import { Buckets } from './buckets.ts';
 import { walk } from './walk.ts';
 
-export function findDuplicateFiles(directory: FilePath, categorizers: Categorizer[]) {
+export async function findDuplicateFiles(
+  directory: FilePath,
+  categorizers: Categorizer[],
+) {
   let buckets = new Buckets();
 
   buckets.add('initial', walk(directory));
 
   for (const categorizer of categorizers) {
-    buckets = categorizer.rebucket(buckets).removeNonDuplicates();
+    buckets = (await categorizer.rebucket(buckets)).removeNonDuplicates();
   }
 
   return buckets;
